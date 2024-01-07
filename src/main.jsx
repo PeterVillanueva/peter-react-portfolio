1e04fc579993c8670929eb114bef1a06c7d89711
PeterVillanueva <peterdonniel4444@gmail.com>
2024-01-07 09:17:27 +0800

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Cursor from "./components/ui/Cursor.jsx";
 import "./index.css";
-import CustomCursor from './components/ui/CustomCursor.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
