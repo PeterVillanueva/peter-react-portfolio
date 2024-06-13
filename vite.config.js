183a2219ed26483ff786d7a1d7f601303755f269
PeterVillanueva <peterdonniel4444@gmail.com>
2024-06-13 18:51:54 +0800

    Fix Deployments

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 let isProduction = process.env.NODE_ENV === 'production';
-let base = isProduction ? '/' : './'; // './' for GitHub Pages, '/' for Vercel
+let base = isProduction ? '/' : '/peter-react-portfolio/'; // './' for GitHub Pages, '/' for Vercel
 
 export default defineConfig({
   plugins: [react()],
