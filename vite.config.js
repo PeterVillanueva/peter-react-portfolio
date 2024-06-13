cc8eac9ca69615be9fd2b344337c704d321f063e
PeterVillanueva <peterdonniel4444@gmail.com>
2024-06-13 18:55:09 +0800

    Fix Issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-let isProduction = process.env.NODE_ENV === 'production';
-let base = isProduction ? '/' : '/peter-react-portfolio/'; // './' for GitHub Pages, '/' for Vercel
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: "/"
 })
